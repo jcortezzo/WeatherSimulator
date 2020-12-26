da5cac01cf4a289cd0ec3c3357d0170d81d4b07f
cortejon <cortejon@cs.washington.edu>
2020-12-26 03:11:54 -0800

    basic stuff

---
+++ WeatherSimulator/Assets/Resources/Scripts/Piece.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Piece : MonoBehaviour
+{
+    ISet<Tile> tilemap;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tilemap = new HashSet<Tile>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Tile t = collision.GetComponent<Tile>();
+        if (t != null)
+        {
+            tilemap.Add(t);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Tile t = collision.GetComponent<Tile>();
+        if (t != null)
+        {
+            tilemap.Remove(t);
+        }
+    }
+}
---
+++ WeatherSimulator/Assets/Resources/Scripts/Tile.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    private ISet<Piece> pieces;
+    [SerializeField] private TileType type;
+
+    private void Awake()
+    {
+        pieces = new HashSet<Piece>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Piece p = collision.GetComponent<Piece>();
+        if (p != null)
+        {
+            pieces.Add(p);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Piece p = collision.GetComponent<Piece>();
+        if (p != null)
+        {
+            pieces.Remove(p);
+        }
+    }
+
+    public void Tic()
+    {
+
+    }
+
+    // TODO: separate effects like tornados from base tiles like default
+    private enum TileType
+    {
+        DEFAULT,
+        WATER,
+        HOT,
+        FIRE,
+        ICE,
+        LIGHTNING,
+        ELECTRIC_WATER,
+        TORNADO,
+    }
+}

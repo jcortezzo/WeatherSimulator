38936299913fc64d9ef13aed8d231f9a6495e678
Nguyen Duong <ducnguyenduchao@gmail.com>
2020-12-26 17:52:44 -0800

    untest bfs

--- WeatherSimulator/Assets/Resources/Scripts/Piece.cs
+++ WeatherSimulator/Assets/Resources/Scripts/Piece.cs
@@ -29,25 +29,83 @@ public class Piece : MonoBehaviour
         int col = board.movementBoard.GetLength(1);
         Vector2 randomPos = new Vector2(Random.Range(0, row), Random.Range(0, col));
 
+        // set up queue and set
         Queue<NextMove> queue = new Queue<NextMove>();
-        NextMove currentPos = new NextMove() { prev = Vector2.zero };
+        ISet<Vector2> visited = new HashSet<Vector2>();
+
+        (int, int) pos = board.enemyLocations[this];
+        NextMove currentPos = new NextMove() { prev = null, nextMove = new Vector2(pos.Item1, pos.Item2) };
+        
+        bool pathFound = false;
+        NextMove lastMove = null;
 
         queue.Enqueue(currentPos);
+        visited.Add(currentPos.nextMove);
+
+        // bfs
         while (queue.Count != 0)
         {
-            
+            NextMove pop = queue.Dequeue();
+
+            if(pop.nextMove == randomPos)
+            {
+                pathFound = true;
+                lastMove = pop;
+                break;
+            }
+            List<Vector2> availableMove = GetNeighbour(currentPos.nextMove, board.movementBoard);
+            foreach(Vector2 nextMove in availableMove)
+            {
+                if(!visited.Contains(nextMove)) {
+                    NextMove newMove = new NextMove() { prev = pop, nextMove = nextMove };
+                    queue.Enqueue(newMove);
+                    visited.Add(newMove.nextMove);
+                }
+            }
         }
 
-        return Vector2.zero;
+        Vector2 result = Vector2.zero; // this might be a problem
+        if(pathFound)
+        {
+            //fence post
+            board.movementBoard[(int)lastMove.nextMove.x, (int)lastMove.nextMove.y] = false;
+            while (lastMove.prev != null) // stop one short
+            {
+                lastMove = lastMove.prev;
+                board.movementBoard[(int)lastMove.nextMove.x, (int)lastMove.nextMove.y] = false;
+            }
+            result = lastMove.nextMove;
+        }
+        return result;
+    }
+
+    private List<Vector2> GetNeighbour(Vector2 current, bool[,] movementBoard)
+    {
+        List<Vector2> result = new List<Vector2>();
+        result.Add( current + Vector2.up);
+        result.Add (current + Vector2.down);
+        result.Add (current + Vector2.left);
+        result.Add (current + Vector2.right);
+        for(int i = 0; i < result.Count; i++)
+        {
+            if(!CanMove(result[i], movementBoard))
+            {
+                result.Remove(result[i]);
+            }
+        }
+        return result;
     }
 
     private class NextMove {
-        public Vector2 prev;
+        public NextMove prev;
+        public Vector2 nextMove;
     }
 
-    private bool CanMove(Vector2 pos, int row, int col)
+    private bool CanMove(Vector2 pos, bool[,] movementBoard)
     {
-        return pos.x >= 0 && pos.x < col && pos.y >= 0 && pos.y < row;
+        int row = movementBoard.GetLength(0);
+        int col = movementBoard.GetLength(1);
+        return pos.x >= 0 && pos.x < col && pos.y >= 0 && pos.y < row & movementBoard[(int)pos.x, (int)pos.y];
     }
 
     public IEnumerator MovePiece(Vector2 newPos)

da313ea75f56cf1456014ba416db981a33e48f84
Ian Wohlers <ianw9@uw.edu>
2020-12-26 23:00:39 -0800

    Add a generic BFS method

--- WeatherSimulator/Assets/Resources/Scripts/Piece.cs
+++ WeatherSimulator/Assets/Resources/Scripts/Piece.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class Piece : MonoBehaviour
 {
@@ -27,65 +28,87 @@ public class Piece : MonoBehaviour
 
     public Vector2Int? GetNextMove(GameBoard board)
     {
-        int row = board.occupiedBoard.GetLength(0);
-        int col = board.occupiedBoard.GetLength(1);
-        Vector2Int randomPos = new Vector2Int(8, 8);
+        var currPos = new Vector2Int(
+            board.enemyLocations[this].Item1,
+            board.enemyLocations[this].Item2
+        );
 
+        // int row = board.occupiedBoard.GetLength(0);
+        // int col = board.occupiedBoard.GetLength(1);
+        Vector2Int randomPos = new Vector2Int(8, 8); // our dest
         Debug.LogFormat("new random pos: {0}, {1}", randomPos.x, randomPos.y);
+
+        // This is a way we could genericize checking for movement instead... other ideas possible
+        Func<Vector2Int, bool> canMove = (Vector2Int pos) =>
+        {
+            int row = board.occupiedBoard.GetLength(0);
+            int col = board.occupiedBoard.GetLength(1);
+            return pos.x >= 0 && pos.x < col && pos.y >= 0 && pos.y < row && !board.occupiedBoard[pos.x, pos.y];
+        };
+
+        var bestPath = GetBestPath(currPos, randomPos, canMove);
+        if (bestPath.Count < 2)
+            return null;
+        return bestPath[1]; // return the NEXT move in our list
+    }
+
+    public List<Vector2Int> GetBestPath(Vector2Int currPos, Vector2Int dest, Func<Vector2Int, bool> canMove)
+    {
         // set up queue and set
         Queue<Vector2Int> queue = new Queue<Vector2Int>();
         // visited is now a mapping of position -> prev
         Dictionary<Vector2Int, Vector2Int?> visited = new Dictionary<Vector2Int, Vector2Int?>();
-        Vector2Int? finalMove = null;
 
-        // enemyLocations should just return Vector2Ints, dw for now
-        var currentPos = new Vector2Int(
-            board.enemyLocations[this].Item1,
-            board.enemyLocations[this].Item2
-        );
-        queue.Enqueue(currentPos);
-        visited.Add(currentPos, null);
+        queue.Enqueue(currPos);
+        visited[currPos] = null;
 
         // bfs
         while (queue.Count != 0)
         {
             var pop = queue.Dequeue();
-            if (pop.Equals(randomPos))
+            if (pop.Equals(dest))
             {
-                Debug.Log("path found");
-                finalMove = pop;
-                break;
+                return ExtractPath(visited, dest);
             }
             foreach (Vector2Int neighbor in GetNeighbours(pop))
             {
-                if (CanMove(neighbor, board.occupiedBoard) && !visited.ContainsKey(neighbor))
+                if (canMove(neighbor) && !visited.ContainsKey(neighbor))
                 {
                     queue.Enqueue(neighbor);
-                    visited.Add(neighbor, pop);
+                    visited[neighbor] = pop;
                 }
             }
         }
-        if (finalMove == null)
+
+        Vector2Int? nextBestDest = null;
+        // if bfs didn't find a path, find a runner up dest
+        foreach (Vector2Int visitedPos in visited.Keys)
         {
-            // Try and find out closest final move
-            foreach (Vector2Int visitedPos in visited.Keys)
-            {
-                var newFinalMove = visited[visitedPos];
-                // Can optimize this later if needed
-                if (finalMove == null ||
-                    ManhattanDistance(finalMove.Value, randomPos) >
-                    ManhattanDistance(newFinalMove.Value, randomPos))
-                    finalMove = newFinalMove;
-            }
+            if (nextBestDest == null ||
+                ManhattanDistance(nextBestDest.Value, dest) >
+                ManhattanDistance(visitedPos, dest))
+                nextBestDest = visitedPos;
         }
-        if (finalMove == null)
-            return null; // No possible moves
-        return ExtractFirstMove(visited, finalMove.Value);
+        if (nextBestDest.HasValue)
+            return ExtractPath(visited, nextBestDest.Value);
+        return new List<Vector2Int>(); // No paths found
     }
 
     private float ManhattanDistance(Vector2Int pos1, Vector2Int pos2)
     {
         return Mathf.Abs(pos1.x - pos2.x) + Mathf.Abs(pos1.y - pos2.y);
+    }
+
+    private List<Vector2Int> ExtractPath(Dictionary<Vector2Int, Vector2Int?> backpointers, Vector2Int dest)
+    {
+        var path = new List<Vector2Int>();
+        while (backpointers[dest] != null)
+        {
+            path.Insert(0, dest);
+            dest = backpointers[dest].Value;
+        }
+        path.Insert(0, dest); // insert our origin into the path
+        return path;
     }
 
     private Vector2Int? ExtractFirstMove(Dictionary<Vector2Int, Vector2Int?> backpointers, Vector2Int dest)

1f8f278498168a8c99878e993817ae989d336392
Nguyen Duong <ducnguyenduchao@gmail.com>
2020-12-27 00:51:50 -0800

    basic ui

---
+++ WeatherSimulator/Assets/Resources/Scripts/UiWeather.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UiWeather : MonoBehaviour
+{
+    [SerializeField] private Weather weather;
+    [SerializeField] private bool selected;
+
+    private Image image;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if(selected)
+    }
+}

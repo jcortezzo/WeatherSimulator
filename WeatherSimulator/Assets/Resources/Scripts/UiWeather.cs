ab14c3c09a13306064049fc5038b1085bafdae34
cortejon <cortejon@cs.washington.edu>
2020-12-29 02:09:12 -0800

    UI more noticeable when changing

--- WeatherSimulator/Assets/Resources/Scripts/UiWeather.cs
+++ WeatherSimulator/Assets/Resources/Scripts/UiWeather.cs
@@ -12,10 +12,20 @@ public class UiWeather : MonoBehaviour
     [SerializeField] private Sprite unselectedSprite;
     [SerializeField] private Image childImage;
     private Image image;
+    RectTransform rectTrans;
+    private Vector3 ogPos;
+    private Vector3 selectPos;
+    private GameObject img;
 
     void Start()
     {
         image = GetComponent<Image>();
+        //rectTrans = GetComponent<RectTransform>();
+        img = transform.Find("Image").gameObject;
+        rectTrans = img.GetComponent<RectTransform>();
+        ogPos = rectTrans.localPosition;
+        selectPos = new Vector2(ogPos.x + 36, ogPos.y);
+        
     }
 
     // Update is called once per frame
@@ -23,6 +33,7 @@ public class UiWeather : MonoBehaviour
     {
         image.color = selected ? Color.yellow : Color.white;
         childImage.sprite = selected ? selectedSprite : unselectedSprite;
+        rectTrans.localPosition = selected ? selectPos : ogPos;
     }
 
 
